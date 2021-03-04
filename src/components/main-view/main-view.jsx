e60c3976758ba7522d46e16ad1276da50dd737b1
TobyRas1992 <tobiasrasmussen@outlook.com>
2021-03-04 09:53:02 +0100

    part 1 of 3.3.

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import axios from 'axios';
+
+class MainView extends React.Component {
+  constructor() {
+    super(); // initialized the state so that I can access its attributes later
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="main-view"></div>
+    );
+  }
+}
