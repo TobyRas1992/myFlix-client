61598f4ae73b931e2536c0f2a2eb6e394064ef69
TobyRas1992 <tobiasrasmussen@outlook.com>
2021-03-05 12:33:23 +0100

    exports class fix

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,7 +4,7 @@ import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 
-class MainView extends React.Component {
+export class MainView extends React.Component {
   constructor() {
     super(); // initialized the state so that I can access its attributes later
     this.state = {
