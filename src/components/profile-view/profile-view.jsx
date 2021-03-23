3cf19b108f53628fa8442936cdc88119abae859c
TobyRas1992 <tobiasrasmussen@outlook.com>
2021-03-23 11:10:11 +0100

    Update profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -19,6 +19,44 @@ export class ProfileView extends React.Component {
   }
 
   render() {
+    const { movies } = this.props;
+    const favoriteMovieList = movies.filter(movie => {
+      return this.state.favoriteMovies.includes(movie._id);
+    });
+
+    return (<React.Fragment>
+      <Container>
+        <h2 className='text-center mb-4 white-words'>Profile Details</h2>
+
+        <Card className="profile-view">
+          <Card.Body>
+            <Card.Text>Username: {this.state.username}</Card.Text>
+            <Card.Text>Email: {this.state.email}</Card.Text>
+            <Card.Text>Birthday: {this.state.birthday}</Card.Text>
+
+            <Link to {`/update`}>
+              <div className="center-btn">
+                <small className="register text-danger ml-2">Update details</small>
+              </div>
+            </Link>
+
+            <Link to {`/`}>
+              <div className="center-btn">
+                <Button className="return-button" variant="info">Return to Movie List</Button>
+              </div>
+            </Link>
+          </Card.Body>
+        </Card>
+      </Container>
+
+      <Container className="my-3">
+        <h2 className="text-center mb-4 white-words">Favorite Movies</h2>
+      </Container>
+
+      <Container ="d-flex row my-3 favorites">
+
+      </Container>
+    </React.Fragment >);
 
   }
 }
